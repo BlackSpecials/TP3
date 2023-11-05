1ffd892248c3a73403ce6d8ba009fcb2dfddb68a
CO1 <honsaliamine04@gmail.com>
2023-11-05 21:30:19 +0100

    Creation des pages des exercices + Parametrage du template

--- src/main/java/jsf/Bean1.java
+++ src/main/java/jsf/Bean1.java
@@ -36,7 +36,8 @@ public class Bean1 {
         }
         return l;
     }
-    public String afficher() {
-    return "affichage_4?nb="+ nombre + "&amp;faces-redirect=true";
-    }
+public String afficher() {
+    return "affichage__4?faces-redirect=true&nb=" + nombre;
+}
+
 }
